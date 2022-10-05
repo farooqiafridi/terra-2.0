85021bc07c0bed095baa5dbf5dbea2e43cfaecab
farooqiafridi <61327719+farooqiafridi@users.noreply.github.com>
2022-10-05 12:25:52 +0530

    env

---
+++ env/variables.tf
@@ -0,0 +1,11 @@
+#variable "rds-secret" { type = string }
+
+variable "rds-username" {
+   type = string 
+   default = "affu"
+}
+
+variable "rds-password" {
+   type = string  
+   default = "0070070007"
+}
